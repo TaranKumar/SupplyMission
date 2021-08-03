ff1869fc39b85e17f7a2aafe08b911f9207c7d3b
TaranKumar <78540263+TaranKumar@users.noreply.github.com>
2021-08-03 15:15:22 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,20 @@
+class box{
+    constructor(x,y,width,height){
+        var options = {
+            "restitution": 0.4,
+            "isStatic":true
+        }
+        
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        rectMode(CENTER);
+        fill(255);
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
